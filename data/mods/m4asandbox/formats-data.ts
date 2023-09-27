56a924c2776f1b0c2a4607b9f7a870b0f6c8725d
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2023-09-26 23:33:27 -0600

    Create formats-data.ts

---
+++ data/mods/m4asandbox/formats-data.ts
@@ -0,0 +1,8 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	// think we learned this was necessary as a bugfix
+	pichuspikyearedmega: {tier: "April Fools"},
+	porygodzmega: {tier: "April Fools"},
+	stoutlandmegau: {tier: "April Fools"},
+	floetteeternalmega: {tier: "April Fools"},
+	meltanmega: {tier: "April Fools"},
+};
