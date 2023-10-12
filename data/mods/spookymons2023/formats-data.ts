8e949436ae04cd8ec6b366e7a7e615aa675f5f3c
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2023-10-11 23:21:49 -0600

    The formatting on this is weirdly spaced
    will fix later

---
+++ data/mods/spookymons2023/formats-data.ts
@@ -0,0 +1,309 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+
+  // 2023 ORIGINAL CONTENT
+
+  // Paul's
+  passmid: { tier: "Spooky" },
+  cindricket: { tier: "Spooky" },
+  dubider: { tier: "Spooky" },
+  harineura: { tier: "Spooky" },
+  incinimite: { tier: "Spooky" },
+
+  // Hematite's
+  wolfsbrain: { tier: "Spooky" },
+  calyrexunova: { tier: "Spooky" },
+  starmiegemini: { tier: "Spooky" },
+
+  // ink's
+  pikachucosplay: { tier: "Spooky" },
+  pikachurockstar: { tier: "Spooky" },
+  pikachubelle: { tier: "Spooky" },
+  pikachupopstar: { tier: "Spooky" },
+  pikachuphd: { tier: "Spooky" },
+  pikachulibre: { tier: "Spooky" },
+  bloonket: { tier: "Spooky" },
+
+  // jazz's
+  tarothwak: { tier: "Spooky" },
+  murkraft: { tier: "Spooky" },
+
+  // 2023 CROSSOVER CONTENT
+
+  blite: { tier: "Spooky" }, // from White Tusk
+
+  araccato: { tier: "Spooky (NFE)" }, // from DLCmons
+  arapeggio: { tier: "Spooky" }, // from DLCmons
+
+  faumbral: { tier: "Spooky (NFE)" }, // from Blindsided
+  fendeerie: { tier: "Spooky" }, // from Blindsided
+
+  snorunt: { tier: "Spooky (NFE)" },
+  glalie: { tier: "Spooky" },
+  froslass: { tier: "Spooky" },
+  froslasstheatrical: { tier: "Spooky" }, // from Evo
+  froslasstheatricalunmasked: { tier: "Illegal" }, // battle-only
+
+  murkrow: { tier: "Spooky (NFE)" },
+  honchkrow: { tier: "Spooky" },
+  honchkrowmega: { tier: "Spooky" }, // from M4A
+
+
+
+  // 2022 CONTENT
+
+	zubat: { tier: "Spooky (NFE)" },
+	golbat: { tier: "Spooky (NFE)" },
+	crobat: { tier: "Spooky" },
+	groabat: { tier: "Spooky" },
+	
+	ralts: { tier: "Spooky (NFE)" },
+	kirlia: { tier: "Spooky (NFE)" },
+	gardevoir: { tier: "Spooky" },
+	gallade: { tier: "Spooky" },
+	possessevoir: { tier: "Spooky" },
+	
+	fealine: { tier: "Spooky" },
+
+	panpour: { tier: "Spooky (NFE)" },
+	simipour: { tier: "Spooky (NFE)" },
+	gorillipour: { tier: "Spooky" },
+
+	drampa: { tier: "Spooky" },
+	drampamegaaged: { tier: "Spooky" },
+	drampamegauntimely: { tier: "Spooky" },
+
+	popplio: { tier: "Spooky (NFE)" },
+	poppliohallowed: { tier: "Spooky (NFE)" },
+	brionne: { tier: "Spooky (NFE)" },
+	brionnehallowed: { tier: "Spooky (NFE)" },
+	primarina: { tier: "Spooky" },
+	primarinahallowed: { tier: "Spooky" },
+
+	charmander: { tier: "Spooky (NFE)" },
+	charmeleon: { tier: "Spooky (NFE)" },
+	charizard: { tier: "Spooky" },
+	charizardhallowed: { tier: "Spooky" },
+
+	turtwig: { tier: "Spooky (NFE)" },
+	grotle: { tier: "Spooky (NFE)" },
+	torterra: { tier: "Spooky" },
+	torterrahallowed: { tier: "Spooky" },
+
+	roserade: { tier: "Spooky" },
+	roseradetrick: { tier: "Illegal" }, // battle-only
+
+	salandit: { tier: "Spooky (NFE)" },
+	salandithallowed: { tier: "Spooky (NFE)" },
+	salazzle: { tier: "Spooky" },
+	salazzlehallowed: { tier: "Spooky" },
+
+	golett: { tier: "Spooky (NFE)" },
+	golurk: { tier: "Spooky" },
+	golurkhallowed: { tier: "Spooky" },
+
+	hauntarant: { tier: "Spooky" },
+
+	necromander: { tier: "Spooky" },
+
+	cozminea: { tier: "Illegal" },
+	cozmineatrue: { tier: "Illegal" },
+	cozmineahallowed: { tier: "Spooky" },
+	cozminevil: { tier: "Illegal" }, // battle-only
+
+	sandile: { tier: "Spooky (NFE)" },
+	krokorok: { tier: "Spooky (NFE)" },
+	krookodile: { tier: "Spooky" },
+	sandilemarsh: { tier: "Spooky (NFE)" },
+	krokorokmarsh: { tier: "Spooky (NFE)" },
+	krookodilemarsh: { tier: "Spooky" },
+
+	pumpkiln: { tier: "Spooky (NFE)" },
+	pumpking: { tier: "Spooky" },
+
+	vulpix: { tier: "Spooky (NFE)" },
+	vulpixalola: { tier: "Spooky (NFE)" },
+	vulpixhoenn: { tier: "Spooky (NFE)" },
+	ninetales: { tier: "Spooky" },
+	ninetalesalola: { tier: "Spooky" },
+	ninetaleshoenn: { tier: "Spooky" },
+	
+	// CCD STUFF
+
+	poultergeist: { tier: "Spooky" },
+	poultergeistheadless: { tier: "Illegal" }, // battle-only
+
+	cobroom: { tier: "Spooky" },
+	cobroomsorcerer: { tier: "Illegal" }, // battle-only
+
+	// EVO STUFF
+	mienfoo: { tier: "Illegal" },
+	mienshao: { tier: "Illegal" },
+	mienshaoyaoguai: { tier: "Spooky" },
+
+	rattata: { tier: "Spooky (NFE)" },
+	raticate: { tier: "Spooky (NFE)" },
+	plaguicate: { tier: "Spooky" },
+
+	hoothoot: { tier: "Spooky (NFE)" },
+	noctowl: { tier: "Spooky (NFE)" },
+	hoobarn: { tier: "Spooky" },
+
+	spinarak: { tier: "Spooky (NFE)" },
+	ariados: { tier: "Spooky (NFE)" },
+	dolorak: { tier: "Spooky" },
+
+	swirlix: { tier: "Spooky (NFE)" },
+	swirlixvariant: { tier: "Spooky (NFE)" },
+	slurpuff: { tier: "Spooky" },
+	slurpuffvariant: { tier: "Spooky" },
+
+	morelull: { tier: "Spooky (NFE)" },
+	shiinotic: { tier: "Spooky" },
+	shiinotickalos: { tier: "Spooky" },
+
+	solrock: { tier: "Spooky (NFE)" },
+	lunatone: { tier: "Spooky (NFE)" },
+	eclipseroid: { tier: "Spooky" },
+
+	duskull: { tier: "Spooky (NFE)" },
+	dusclops: { tier: "Spooky (NFE)" },
+	dusknoir: { tier: "Spooky" },
+	dusglow: { tier: "Spooky" },
+
+	noibat: { tier: "Spooky (NFE)" },
+	noivern: { tier: "Spooky" },
+	noivernvariant: { tier: "Spooky" },
+
+	drowzee: { tier: "Spooky (NFE)" },
+	hypno: { tier: "Spooky (NFE)" },
+	mezmir: { tier: "Spooky" },
+
+	shelmet: { tier: "Spooky (NFE)" },
+	accelgor: { tier: "Spooky (NFE)" },
+	velocinobi: { tier: "Spooky" },
+
+	karrablast: { tier: "Spooky (NFE)" },
+	escavalier: { tier: "Spooky" },
+	escavaliervariant: { tier: "Spooky" },
+
+	michu: { tier: "Spooky (NFE)" },
+	mimikyu: { tier: "Spooky" },
+
+	cubone: { tier: "Spooky (NFE)" },
+	marowak: { tier: "Spooky (NFE)" },
+	resurrectric: { tier: "Spooky" },
+
+	yamask: { tier: "Spooky (NFE)" },
+	yamaskgalar: { tier: "Spooky (NFE)" },
+	cofagrigus: { tier: "Spooky" },
+	runerigus: { tier: "Spooky" },
+	cofagrigusunbound: { tier: "Spooky" },
+
+	horsea: { tier: "Illegal" },
+	horseavariant: { tier: "Spooky (NFE)" },
+	seadra: { tier: "Illegal" },
+	seadravariant: { tier: "Spooky (NFE)" },
+	tyrandra: { tier: "Spooky" },
+
+	ufogepi: { tier: "Spooky (NFE)" },
+	ufogetic: { tier: "Spooky (NFE)" },
+	ufogekiss: { tier: "Spooky" },
+
+	klefki: { tier: "Spooky" },
+	klefkigalar: { tier: "Spooky" },
+	klefkigalarrevealed: { tier: "Illegal" }, //battle-only
+
+	// M4A STUFF
+
+	poochyena: { tier: "Spooky (NFE)" },
+	mightyena: { tier: "Spooky" },
+	mightyenamega: { tier: "Spooky" },
+
+	zigzagoon: { tier: "Spooky (NFE)" },
+	zigzagoongalar: { tier: "Spooky (NFE)" },
+	linoone: { tier: "Spooky" }, // is it really that spooky?
+	linoonegalar: { tier: "Spooky (NFE)" },
+	obstagoon: { tier: "Spooky" },
+	obstagoonmega: { tier: "Spooky" },
+
+	litwick: { tier: "Spooky (NFE)" },
+	lampent: { tier: "Spooky (NFE)" },
+	chandelure: { tier: "Spooky" },
+	chandeluremega: { tier: "Spooky" },
+
+	fennekin: { tier: "Spooky (NFE)" },
+	braixen: { tier: "Spooky (NFE)" },
+	delphox: { tier: "Spooky" },
+	delphoxmega: { tier: "Spooky" },
+
+	misdreavus: { tier: "Spooky (NFE)" },
+	mismagius: { tier: "Spooky" },
+	mismagiusmega: { tier: "Spooky" },
+
+	noivernmega: { tier: "Spooky" }, // other two stages are under Evo
+
+	// CANON
+
+	croagunk: { tier: "Spooky (NFE)" },
+	toxicroak: { tier: "Spooky" },
+
+	zorua: { tier: "Spooky (NFE)" },
+	zoruahisui: { tier: "Spooky (NFE)" },
+	zoroark: { tier: "Spooky" },
+	zoroarkhisui: { tier: "Spooky" },
+
+	applin: { tier: "Spooky (NFE)" },
+	flapple: { tier: "Spooky" },
+	appletun: { tier: "Spooky" },
+	dipplin: { tier: "Spooky (NFE)" },
+
+	dewpider: { tier: "Spooky (NFE)" },
+	araquanid: { tier: "Spooky" },
+
+	spritzee: { tier: "Spooky (NFE)" },
+	aromatisse: { tier: "Spooky" },
+
+	stufful: { tier: "Spooky (NFE)" },
+	bewear: { tier: "Spooky" },
+
+	joltik: { tier: "Spooky (NFE)" },
+	galvantula: { tier: "Spooky" },
+
+	hatenna: { tier: "Spooky (NFE)" },
+	hattrem: { tier: "Spooky (NFE)" },
+	hatterene: { tier: "Spooky" },
+
+	impidimp: { tier: "Spooky (NFE)" },
+	morgrem: { tier: "Spooky (NFE)" },
+	grimmsnarl: { tier: "Spooky" },
+
+	purrloin: { tier: "Spooky (NFE)" },
+	liepard: { tier: "Spooky" },
+
+	riolu: { tier: "Spooky (NFE)" },
+	lucario: { tier: "Spooky" },
+
+	rockruff: { tier: "Spooky (NFE)" },
+	lycanrocmidnight: { tier: "Spooky" },
+
+	mimejr: { tier: "Spooky (NFE)" },
+	mrmime: { tier: "Spooky" },
+	mrmimegalar: { tier: "Spooky (NFE)" },
+	mrrime: { tier: "Spooky" },
+
+	shedinja: { tier: "Spooky" },
+
+	slowkinggalar: { tier: "Spooky" },
+
+	woobat: { tier: "Spooky (NFE)" },
+	swoobat: { tier: "Spooky" },
+
+  pumpkaboo: { tier: "Spooky (NFE)" },
+  pumpkaboosmall: { tier: "Spooky (NFE)" },
+  pumpkaboolarge: { tier: "Spooky (NFE)" },
+  pumpkaboosuper: { tier: "Spooky (NFE)" },
+	gourgeist: { tier: "Spooky" },
+	gourgeistsmall: { tier: "Spooky" },
+	gourgeistlarge: { tier: "Spooky" },
+	gourgeistsuper: { tier: "Spooky" },
+};
