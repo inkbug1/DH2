c5fec2db11f7b6c94e41dc128d704b41e5b21247
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2023-10-13 19:14:46 -0600

    Update moves.ts

--- data/mods/spookymons2023/moves.ts
+++ data/mods/spookymons2023/moves.ts
@@ -1,5 +1,7 @@
+// import {deepClone} from '../../../sim/dex'; // I *might* not need this, since I'm using this.dex.deepClone
+
 export const Moves: {[moveid: string]: ModdedMoveData} = {
-	banefultransformation: { // WIP
+	banefultransformation: {
 		num: -2001, // 2023 content
 		accuracy: true,
 		basePower: 0,
@@ -8,7 +10,33 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 		name: "Baneful Transformation",
 		pp: 20,
 		priority: 0,
-		flags: {}, // can't be Snatched or anything
+		flags: {failencore: 1, failmefirst: 1, nosleeptalk: 1, noassist: 1, failcopycat: 1, failinstruct: 1, failmimic: 1},
+		onTry(source) {
+			let werewolf = null;
+			for (let i = source.side.pokemon.length - 1; i >= 0; i--) {
+				const possibleTarget = source.side.pokemon[i];
+				if (possibleTarget.wolfsbane && possibleTarget !== source) {
+					werewolf = possibleTarget;
+					break;
+				}
+			}
+			if (werewolf) {
+				if (werewolf.fainted) {
+					this.hint(`Your team's Baneful Transformation is ${werewolf.name}, who has already been defeated!`, true, source.side);
+					return false;
+				}
+				if (werewolf.isActive) this.hint(`Your team's Baneful Transformation is ${werewolf.name}, who is already active!`, true, source.side);
+					return false;
+				}
+				return !!this.switchIn(werewolf, source.position, 'banefultransformation', true);
+			}
+			if (this.canSwitch(source.side)) {
+				this.hint(`Select a Pokémon to become your team's Baneful Transformation!`, true, source.side);
+				this.hint(`From now on, when you use Baneful Transformation, you'll only be able to switch to the Pokémon you choose now.`, true, source.side);
+			}
+			return !!this.canSwitch(source.side);
+		},
+		selfSwitch: true,
 		// TO DO:
 		// first use: use move.selfSwitch to pick a target, record that target
 		// subsequent uses: force to switch to the same target every time; the move fails if it can't
@@ -19,49 +47,40 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 			this.add('-anim', source, "Moonlight", source);
 		},
 		condition: {
-			// I think Neurotoxin will be handled internally as a field effect,
-			// as I can't find any way to check the source of an active move from the target's point of view
-
-			// mechanical distinctions of the monster (not sure if any or all of these will be kept)
-			onTryHeal(damage, target, source, effect) { // reduce healing
-				if (target.illusion) return this.chainModify([2, 3]);
-			},
+			// mechanical distinctions of the monster (not at all sure if any or all of these will be kept)
 			onSourceModifyDamage(damage, source, target, move) {
-				if (target.illusion && !(target.getMoveHitData(move).typeMod > 0)) { // reduce damage taken
+				if (!(target.getMoveHitData(move).typeMod > 0)) { // reduce damage taken
 					return this.chainModify([2, 3]); // this might be too extreme, but we can come back to it
 				}
 			},
+			onTryHeal(damage, target, source, effect) { // reduce healing
+				return this.chainModify([2, 3]); // this is meant to even out with the reducted damage taken
+			},
 			onModifySpe(spe, pokemon) { // reduce Speed
-				if (pokemon.illusion) return this.chainModify(0.5);
+				return this.chainModify(0.5);
 			},
 
 			// should appear as a monster
 			onBeforeSwitchIn(pokemon) {
-				pokemon.illusion = null;
-				if (pokemon.m.wolfsbane) { // should be the Pokémon that used Baneful Transformation
-					pokemon.illusion = pokemon.m.wolfsbane;
-					pokemon.m.backup.gender = pokemon.m.wolfsbane.gender;
-					pokemon.m.backup.name = pokemon.m.wolfsbane.name;
-					pokemon.m.backup.species = pokemon.m.wolfsbane.species;
-					pokemon.illusion.gender = '';
-					pokemon.illusion.name = '???';
-					pokemon.illusion.species = {
-						id: 'banefultransformation',
-						name: 'Baneful Transformation',
-						forme: '',
-						types: ["???"],
-						abilities: {0: 'Neurotoxin'},
-					};
-				}
+				pokemon.illusion = this.dex.deepClone(pokemon); // will this work? I hope so
+				pokemon.illusion.gender = '';
+				pokemon.illusion.name = '???';
+				pokemon.illusion.species = {
+					id: 'banefultransformation',
+					name: 'Baneful Transformation',
+					forme: '',
+					types: ["???"],
+				};
 			},
 			onStart(pokemon) {
+				if (pokemon.illusion) pokemon.illusion.name = 'The Baneful Transformation'; // should still appear as ??? on the health bar, I hope!
 				this.add('-message', `Grrrrrr...`);
 				// this.add(`raw|<img src="IMAGE URL HERE" height="14" width="32">`); // would be fun to add a visual here :D (but I haven't made one yet P:)
-				if (!pokemon.m.werewolfHints && pokemon.m.wolfsbane && pokemon.m.backup.name) {
-					this.hint(`This must be one of ${pokemon.m.backup.name}'s allies, but which one?`);
+				if (!pokemon.m.werewolfHints && pokemon.m.wolfsbane && pokemon.m.wolfsbane.name) {
+					this.hint(`This must be one of ${pokemon.m.wolfsbane.name}'s allies, but which one?`);
 					this.hint(`The Baneful Transformation's identity is a secret. It's impossible to tell what moves it uses—or even how much HP it has left!`);
 					this.hint(`The Baneful Transformation will take less damage from moves, unless they're super effective. However, its Speed and HP recovery are also reduced.`);
-					this.hint(`During this battle, ${pokemon.m.backup.name} will always summon the same Baneful Transformation. You should try to deduce its identity over the course of the battle!`);
+					this.hint(`During this battle, ${pokemon.m.wolfsbane.name} will always summon the same Baneful Transformation. You should try to deduce its identity over the course of the battle!`);
 					pokemon.m.werewolfHints = true;
 				}
 				this.add('-ability', pokemon, 'Neurotoxin');
@@ -69,28 +88,20 @@ export const Moves: {[moveid: string]: ModdedMoveData} = {
 			},
 			// tiny bit of backup to make sure the original Wolfsbrain's properties aren't messed with... should test to see if this matters!
 			onSwitchOut(pokemon) {
-				pokemon.illusion.gender = pokemon.m.backup.gender;
-				pokemon.illusion.name = pokemon.m.backup.name;
-				pokemon.illusion.species = pokemon.m.backup.species;
 				pokemon.illusion = null;
 			},
 			onFaint(pokemon) {
-				pokemon.illusion.gender = pokemon.m.backup.gender;
-				pokemon.illusion.name = pokemon.m.backup.name;
-				pokemon.illusion.species = pokemon.m.backup.species;
 				pokemon.illusion = null;
 				this.add('-end', pokemon, 'Illusion');
-				this.add('-message', `The monster was ${pokemon.name}!`);
+				this.add('-message', `The Baneful Transformation was ${pokemon.name}!`);
 			},
 
 			// moves hidden while appearing as a monster
 			onModifyMove(move, source, target) {
-				if (source.illusion) {
-					move.name = 'an unknown move';
-				}
+				move.name = 'an unknown move';
 			},
 			onPrepareHit(target, source, move) {
-				if (source.illusion && source === this.effectState.target) {
+				if (source === this.effectState.target) {
 					this.attrLastMove('[still]');
 					this.add('-anim', source, "Mist", source);
 				}
